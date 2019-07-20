649fa32f9f7a5e28fbcae76008c008e5ae1798a6
Owner <owner@MA4-0064.local>
2019-07-20 21:09:11 +0900

    consoleとコメントアウト削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,15 +54,15 @@ $(function(){
         contentType: false
       })
        .done(function(data){
-        console.log("done")
+
          var html = buildHTML(data);
          $('.messages').append(html);      
          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
          $('form')[0].reset();
        })
        .fail(function(){
-        alert('どんまい');
+        alert('error');
        });
-    //   return false;
+
     });
 })
