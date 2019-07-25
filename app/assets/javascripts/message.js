fe740464f51373ca7cf7f1367cfb2e1dd7bdfd37
Owner <owner@MA4-0064.local>
2019-07-25 17:46:10 +0900

    文章短縮

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,7 +10,7 @@ $(function(){
                 ${message.user_name}
                 </div>
                 <div class="upper-message__date">
-                ${message.date}
+                ${message.created_at}
                 </div>
             </div>
             <div class="lower-message">
@@ -22,7 +22,6 @@ $(function(){
             </div>`
         return html;
     }
-
     $(".new_message").on("submit", function(e){
         e.preventDefault();
       var formData = new FormData(this);
@@ -47,72 +46,15 @@ $(function(){
        })
 
        });
-   
-var buildMessageHTML = function(message) {
-    if (message.content && message.image.url) {
-    var html = `<div class="message" data-id="${message.id}">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">
-                    ${message.user_name}
-                  </div>
-                  <div class="upper-message__date">
-                    ${message.created_at}
-                  </div>
-                  </div>
-                  <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${message.content}
-                  </p>
-                  <img class="lower-message__image"${message.image.url}">
-                  </div>
-                </div>`
-    } else if (message.content) {
-      var html = `<div class="message" data-id="${message.id}">
-                    <div class="upper-message">
-                    <div class="upper-message__user-name">
-                    ${message.user_name}
-                    </div>
-                    <div class="upper-message__date">
-                    ${message.created_at}
-                    </div>
-                    </div>
-                    <div class="lower-message">
-                    <p class="lower-message__content">
-                    ${message.content}
-                    </p>
-                    </div>
-                    </div>`
-    } else if (message.image.url) {
 
-      var html = `<div class="message" data-id="${message.id}">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                     <img class="lower-message__image"${message.image.url}">
-                    </div>
-                  </div>`
-    };
-    return html;
-  };
 
     var reloadMessages = function() {
-        //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
         last_message_id = $('.message:last').data('id');
         if (window.location.href.match(/\/groups\/\d+\/messages/)){
-
         $.ajax({
-          //ルーティングで設定した通りのURLを指定
           url: 'api/messages',
-          //ルーティングで設定した通りhttpメソッドをgetに指定
           type: 'get',
           dataType: 'json',
-          //dataオプションでリクエストに値を含める
           data: {id: last_message_id}
         })
         .done(function(messages) {
