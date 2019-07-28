7a3738b286b4ae2fedbb37d1043a9a6d40c86068
Owner <owner@MA4-0064.local>
2019-07-28 18:28:58 +0900

    storageとバケット名を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'techexpertseptember'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bucket'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/techexpertseptember'
 end
